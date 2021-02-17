457bf8bdc70ba6648479cd6101ca877d10424845
Bryan Teng <bteng1226@gmail.com>
2021-02-17 13:59:13 -0500

    story shows more information and added search feature

--- src/components/story.js
+++ src/components/story.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Story({props}){
   const {author, content, description, publishedAt, source, title, url, urlToImage} = props
 
+  const [showContent, setShowContent] = useState(false)
+
+  const handleTitleClick = () =>{
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  const handleStoryClick = () => {
+    setShowContent(current => !current)
+  }
+
+
   return(
-    <div class="story">
-      {title}
+    <div className="story" onClick={handleStoryClick} >
+      <h1 className="storyTitle" onClick={handleTitleClick}>{title}</h1>
+
+      {author && author.length > 0  ? <div className="storyAuthor">by:{author}</div> : null }
+      {source.name && source.name.length > 0  ? <div className="storySource">Source:{source.name}</div> : null }
+
+      <div className="storyPublishedAt">date: {publishedAt}</div>
+      <div className="storyDescription">description: {description}</div>
+      {showContent ? <p>{content}</p> : null}
     </div>
   )
 }
--- src/containers/newsboard.js
+++ src/containers/newsboard.js
@@ -6,25 +6,40 @@ function News(){
 
   const [news, setNews] = useState([])
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("")
 
   useEffect(()=>{
     const news_api_key = process.env.REACT_APP_NEWS_API_KEY
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${news_api_key}`)
+    let endpoint = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${news_api_key}`
+
+    if(query != "") endpoint = `https://newsapi.org/v2/everything?q=${query}&apiKey=${news_api_key}`
+
+    fetch(endpoint)
     .then(res => res.json())
     .then(data => {
-      // console.log(data)
       setNews(data.articles)
       setIsLoading(false)
-      console.log(news,"the news")
     }).catch(error => console.log(error));
   },[isLoading])
 
+  const handleSubmit = (event) =>{
+    setIsLoading(true)
+    event.preventDefault()
+  }
 
 return(
   <div className="Newsboard">
-    News board
-    {isLoading ? <div>News board loading</div> :
-      <Fragment class="Newsboard">
+    <h1>News board</h1>
+    <br/>
+    <div className="Search">
+      <label>Search:</label>
+      <form onSubmit={handleSubmit}>
+        <input value={query} placeholder="search news here" onChange={(event)=>setQuery(event.target.value)}/>
+      </form>
+    </div>
+
+    {news.length == 0 ? <div>News board loading</div> :
+      <Fragment>
         {news.map(paper => <Story id={paper.url} props={paper} /> )}
       </Fragment>
   }
